3bcd53e7c643101580380f5b93818d5b0e9bafcd
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2023-07-22 01:02:36 +0100

    deployement quick fix

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
-const AIPicker = () => {
+import CustomButton from './CustomButton';
+const AIPicker = ({prompt,setPrompt,generateImg,handleSubmit}) => {
   return (
-    <div>AIPicker</div>
+    <div className='aipicker-container'>
+      <textarea 
+      className= {`aipicker-textarea placeholder:text-cyan-400`}
+      rows={5}
+      value={prompt}
+      onChange={(e) => setPrompt(e.target.value)}
+      placeholder='Ask AI for image generation...'
+      />
+      <div className='flex flex-wrap gap-3'>
+        {generateImg? (
+          <CustomButton
+          type={'outline'}
+          title={'Asking AI...'}
+          customStyle={'text-xs'}
+          />
+        ):(
+         <>
+          <CustomButton 
+          type={'outline'}
+          title={'AI Logo'}
+          handleClick={()=>handleSubmit('logo')}
+          customStyle={'text-xs'}
+          />
+
+          <CustomButton 
+          type={'filled'}
+          title={'AI Full'}
+          handleClick={()=>handleSubmit('full')}
+          customStyle={'text-xs '}
+          />
+         </>
+        )}
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -50,7 +50,19 @@ const Customizer = () => {
   const handleSubmit = async(type) => {
     if(!prompt) return alert('Please enter a prompt');
     try {
-      // call our server to generate image
+      setGeneratingImg(true);
+      const response = await fetch('http://localhost:8080/api/v1/dalle',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt
+        })
+      });
+
+      const data = await response.json();
+      handleDecals(type,`data:image/png;base64,${data.photo}`)
     } catch (error) {
       alert(error)
     }finally{
